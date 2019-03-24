ec6c94cdbfa310bd4f81393c3f2cd487a497009e
kritika-gupta <kritika29498@gmail.com>
2019-03-24 22:27:56 +0530

    Completed Functionality (Serial Code)

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h> 
 #include <algorithm>
 using namespace std;
-int seed = time(NULL);
 
 class Board
 {
@@ -160,7 +159,9 @@ class State
     // constructor
     State(){
         Board board;
-        this->board = board;
+        this->board.setBoardValues(board.getBoardValues());
+        this->board.setMoves(board.getMoves());
+
         this->player = 0;
         this->visitCount = 0;
         this->winScore = 0;
@@ -205,7 +206,10 @@ class State
     }
     void addScore(double w)
     {
-        this->winScore = w;
+        if(this->winScore!=INT_MIN){
+            this->winScore += w;
+
+        }
     }
     // getter functions
     Board* getBoard()
@@ -238,11 +242,6 @@ class State
             newState.getBoard()->newMove(newState.getPlayer(), emptyPositions[i].first, emptyPositions[i].second);
 
             possibleStates.push_back(newState);
-
-            //cout<<"new possible state is "<<endl;
-            //newState.getBoard()->display();
-            cout<<endl<<endl;
-
         }
         return possibleStates;
     }
@@ -261,7 +260,7 @@ class State
         if(totalPos==0) return;
 
         
-        std::mt19937_64 random_engine(seed);
+        std::mt19937_64 random_engine(time(NULL));
         std::uniform_int_distribution<int> dis(0, totalPos-1);
         int randnum = dis(random_engine);
         this->board.newMove(this->player, emptyPos[randnum].first, emptyPos[randnum].second);
@@ -283,7 +282,11 @@ class Node
     Node()
     {
         State s;
-        this->state = s;
+        this->state.setBoard(s.getBoard());
+        this->state.setPlayer(s.getPlayer());
+        this->state.setWinScore(s.getWinScore());
+        this->state.setVisitCount(s.getVisitCount());
+
         this->parent=nullptr;
         vector<Node> children(0);
         this->children = children;
@@ -370,7 +373,7 @@ class Node
     Node* getRandomChild()
     {
         int numChild = this->children.size();
-        std::mt19937_64 random_engine(seed);
+        std::mt19937_64 random_engine(time(NULL));
         std::uniform_int_distribution<int> dis(0, numChild-1);
         return &(this->children[dis(random_engine)]);
         
@@ -395,9 +398,9 @@ class Node
             Node* child = &(*this->getChildren())[i];
             double u = calcUCB(child->getState()->getWinScore(), child->getState()->getVisitCount(), this->getState()->getVisitCount());
             child->setUCB(u);
-            cout<<"ucb ="<<u<<endl;
-            child->getState()->getBoard()->display();
-            cout<<endl;
+            //cout<<"ucb ="<<u<<endl;
+            //child->getState()->getBoard()->display();
+            //cout<<endl;
 
             
 
@@ -407,9 +410,9 @@ class Node
             }
         }
         // return the child with max ucb score
-        cout<<"Returning from findBestChild with : "<<endl;
-        bestChild->getState()->getBoard()->display();
-        cout<<endl<<endl;
+        //cout<<"Returning from findBestChild with : "<<endl;
+        //bestChild->getState()->getBoard()->display();
+        
         return bestChild ;
 
     }
@@ -421,14 +424,14 @@ class Node
         // return child with highest visit score
         for(int i=0; i<this->children.size(); i++){
             Node* child = &(this->children[i]);
-            cout<<"In getChildWithMaxScore, child "<<i<<" is"<<endl;
-            child->getState()->getBoard()->display();
-            cout<<" with vistis = "<<child->getState()->getVisitCount()<<endl;
+            //cout<<"In getChildWithMaxScore, child "<<i<<" is"<<endl;
+            //child->getState()->getBoard()->display();
+            //cout<<" with vistis = "<<child->getState()->getVisitCount()<<endl;
             if((child->getState()->getVisitCount())>max){
                 best = child;
                 max = child->getState()->getVisitCount();
             }
-            cout<<"max is"<<max<<endl;
+            //cout<<"max is"<<max<<endl;
         }
         return best;
 
@@ -442,32 +445,34 @@ class Node
 
 class Tree
 {
-    Node root;
+    Node* root;
     public:
     //constructor
     Tree()
     {
-        Node r;
-        this->root = r;
+        cout<<"tree contstructor"<<endl;
+        
+        this->root = new Node();
     }
     
     Node* getRoot()
     {
-        return &(this->root);
+        return (this->root);
     }
     void setRoot(Node* p)
     {
-        this->root = *p;
+        this->root = p;
     }
 
     void dispTree()
     {
         cout<<"root node : "<<endl;
-        this->root.printNode();
-        for(int i=0; i<this->root.getChildren()->size(); i++)
+        this->root->printNode();
+        for(int i=0; i<this->root->getChildren()->size(); i++)
         {
             cout<<"child node :"<<endl;
-            (*this->root.getChildren())[i].printNode();
+            (*this->root->getChildren())[i].printNode();
+            cout<<"score = "<<(*this->root->getChildren())[i].getState()->getWinScore()<<endl<<endl;
 
         }
     }
@@ -478,36 +483,41 @@ class Tree
 class MCTS
 {
     static int const WIN_SCORE = 100;
+    Tree* tree;
 
     public:
+    MCTS()
+    {
+        this->tree = new Tree();
+    }
+
+    
     Board findNextMove(Board* board, int player){
         int opponent = 1 - player;
         cout<<"opponent for this mcts object is "<<opponent<<endl;
         // create a new tree to find the next move
-        Tree tree;
-        Node* rootNode = tree.getRoot(); 
+        Node* rootNode = this->tree->getRoot(); 
 
         //set the given board (current game state) as the root's state's board
         rootNode->getState()->setBoard(board);
         rootNode->getState()->setPlayer(opponent);
-        cout<<"ROOT IS"<<endl;
-        rootNode->getState()->getBoard()->display();
+        //cout<<"ROOT IS"<<endl;
+        //rootNode->getState()->getBoard()->display();
         cout<<endl<<endl;
         //run the 4 steps for a set number of iterations
-        int nIter = 900;
+        int nIter = 1e6;
         cout<<"starting iterations"<<endl;
         for(int i=0; i < nIter; i++)
         {
-            cout<<"iteration number "<<i<<endl;
             Node* bestNode = rootNode;
             // SELECTION
             // select a path to leaf node with best UCB
             if(rootNode->getChildren()->empty()==false){
-                cout<<"Root has"<<rootNode->getChildren()->size()<<" children for selection"<<endl;
+                //cout<<"Root has"<<rootNode->getChildren()->size()<<" children for selection"<<endl;
                 bestNode = selectNode(rootNode);
             }    
             
-            cout<<"Selection done"<<endl;
+            //cout<<"Selection done"<<endl;
             //bestNode->getState()->getBoard()->display();
             // check if selected node is not end game scenario
             // EXPAND
@@ -515,11 +525,11 @@ class MCTS
 
             // status of best node
             int temp = bestNode->getState()->getBoard()->checkStatus();
-            cout<<"After selection, we selected a node with status = "<<temp<<endl<<endl;
+            //cout<<"After selection, we selected a node with status = "<<temp<<endl<<endl;
             if(temp==2){
                 expandNode(bestNode);
             }
-            cout<<"Expansion done"<<endl;
+            //cout<<"Expansion done"<<endl;
 
 
             // SIMULATE
@@ -528,28 +538,27 @@ class MCTS
             if(bestNode->getChildren()->size() > 0){
                 nodeExp = bestNode->getRandomChild();
             }
-            cout<<"Got a random child done"<<endl;
-            cout<<"The random Child is "<<endl;
-            nodeExp->getState()->getBoard()->display();
-            cout<<endl<<endl;
+            //cout<<"Got a random child done"<<endl;
 
+            //cout<<"The random Child is "<<endl;
+            //nodeExp->getState()->getBoard()->display();
+            //cout<<endl<<endl;
             // simulate a random playout of the random child
             int playoutRes = simulateRandomPlayout(nodeExp);
-            cout<<"simulation random playout done"<<endl;
-
 
             // UPDATE VIA BACKPROP
             backProp(nodeExp, playoutRes);
-            cout<<"BAckprop done"<<endl;
+            //cout<<"BAckprop done"<<endl;
 
         }
         Node* temp = rootNode->getChildWithMaxScore();
         
         Node winner(temp);
-        //tree.setRoot(temp);
+        this->tree->dispTree();
+        this->tree->setRoot(temp);
         //tree.dispTree();
-                cout<<"returned from getChildWithMAxScore and temp has: "<<endl;
-                        temp->getState()->getBoard()->display();
+        //cout<<"returned from getChildWithMAxScore and temp has: "<<endl;
+        //temp->getState()->getBoard()->display();
 
 
         return *(winner.getState()->getBoard());
@@ -568,12 +577,11 @@ class MCTS
 
             node = node->findBestChild();
             num = node->getChildren()->size();
-            cout<<endl<<endl<<"returned selection child has "<< num <<" children"<<endl;
 
         }
-        cout<<"returning from selection by selecting : "<<endl;
-        node->getState()->getBoard()->display();
-        cout<<endl<<endl;
+        //cout<<"returning from selection by selecting : "<<endl;
+        //node->getState()->getBoard()->display();
+        //cout<<endl<<endl;
         return node;
     }
 
@@ -581,23 +589,22 @@ class MCTS
     {
         vector<State> possibleStates = node->getState()->getPossibleStates();
         // for each possible state, create a new child node
-        cout<<"Number of possible states are"<<possibleStates.size()<<endl;
+        //cout<<"Number of possible states are"<<possibleStates.size()<<endl;
         for(int i =0; i<possibleStates.size(); i++){
             Node newNode(&possibleStates[i]);
             newNode.setParent(node);
             newNode.getState()->setPlayer(1-node->getState()->getPlayer());
             node->getChildren()->push_back(newNode);
 
-            cout<<endl<<endl;
+            
         }
-        cout<<"after expanding, root has "<<node->getChildren()->size()<<" children"<<endl;
     }
 
     void backProp(Node* nodeExp, int player)
     {
-        cout<<"In back prop with player"<<player<<endl;
-        cout<<"Starting from node :"<<endl;
-        nodeExp->getState()->getBoard()->display();
+        // cout<<"In back prop with player"<<player<<endl;
+        // cout<<"Starting from node :"<<endl;
+        // nodeExp->getState()->getBoard()->display();
         Node *tempNode = nodeExp;
         while(tempNode!=nullptr)
         {
@@ -614,28 +621,29 @@ class MCTS
 
     int simulateRandomPlayout(Node* node)
     {
-        int boardStatus = node->getState()->getBoard()->checkStatus();
+        Node tempNode = (*node);  // copy of node to be played out
+        State tempState = tempNode.getState();
+        int boardStatus = tempState.getBoard()->checkStatus();
         
-        cout<<"In simulate random playout, board status is "<<boardStatus<<endl;
+        //cout<<"In simulate random playout, board status is "<<boardStatus<<endl;
         if(boardStatus == 1 - node->getState()->getPlayer())
         {
             //opponent has won, end playout
             cout<<"opponent won"<<endl;
-            Node *t = node->getParent();
-            t->getState()->setWinScore((double)INT_MIN);
-            cout<<"Returning from simulation"<<endl;
+            tempNode.getParent()->getState()->setWinScore((double)INT_MIN);
+            //cout<<"Returning from simulation"<<endl;
             return boardStatus;
         }
 
         // while board is in progress
-        State* tempState = (node->getState());
         while(boardStatus == 2)
         {
             //cout<<"Board in prog"<<endl;
-            tempState->togglePlayer();
+            tempState.togglePlayer();
             //cout<<"going to random play"<<endl;
-            tempState->randomPlay();
-            boardStatus = tempState->getBoard()->checkStatus();
+            tempState.randomPlay();
+            
+            boardStatus = tempState.getBoard()->checkStatus();
         }
 
         return boardStatus;
@@ -668,6 +676,7 @@ int main(){
         }
         cout<<"game continues"<<endl;
         player = 1-player;
+        
 
     }
     int winStat = myboard.checkStatus();
